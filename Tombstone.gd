7e8b74bc9c9519cac7d9e175589ebaf8b9fe2564
aravash2 <Jacob2.Woodman@live.uwe.ac.uk>
2020-08-05 21:04:39 +0100

    added level 2
    tombstones breaking too

--- Tombstone.gd
+++ Tombstone.gd
@@ -4,4 +4,6 @@ export(bool) var one_time = false
 
 func _on_hurtbox_area_entered(area):
 	area.get_parent().get_parent().create_memory()
-	if one_time: $hurtbox.queue_free()
+	if one_time:
+		$hurtbox.queue_free()
+		$AnimationPlayer.play("broken")
